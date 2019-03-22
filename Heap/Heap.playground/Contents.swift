446a976324ed5e1fa11b6c1d810664dd161d953f
chai <bravetsia@gmail.com>
2019-03-22 16:11:12 +0800

    Heap shiftUp() shiftDown()

--- Heap/Heap.playground/Contents.swift
+++ Heap/Heap.playground/Contents.swift
@@ -16,7 +16,7 @@ struct Heap<T> {
     private mutating func configureHeap(from array: [T]) {
         nodes = array
         for i in stride(from: (nodes.count/2-1), through: 0, by: -1) {
-
+            shiftDown(i)
         }
     }
 
@@ -44,6 +44,49 @@ struct Heap<T> {
         return nodes.first
     }
 
+    public mutating func insert(_ value: T) {
+        nodes.append(value)
+        shiftUp(nodes.count - 1)
+    }
+
+    public mutating func insert<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        for value in sequence {
+            insert(value)
+        }
+    }
+
+    public mutating func replace(index i: Int, value: T) {
+        guard i < nodes.count else { return }
+
+        remove(at: i)
+        insert(value)
+    }
+
+    @discardableResult public mutating func remove() -> T? {
+        guard !nodes.isEmpty else { return nil }
+
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        } else {
+            let value = nodes[0]
+            nodes[0] = nodes.removeLast()
+            shiftDown(0)
+            return value
+        }
+    }
+
+    @discardableResult public mutating func remove(at index: Int) -> T? {
+        guard index < nodes.count else { return nil }
+
+        let size = nodes.count - 1
+        if index != size {
+            nodes.swapAt(index, size)
+            shiftDown(from: index, until: size)
+            shiftUp(index)
+        }
+        return nodes.removeLast()
+    }
+
     mutating func shiftUp(_ index: Int) {
         var childIndex = index
         let child = nodes[childIndex]
@@ -57,4 +100,44 @@ struct Heap<T> {
 
         nodes[childIndex] = child
     }
+
+    mutating func shiftDown(from index: Int, until endIndex: Int) {
+        let leftChildIndex = self.leftChildIndex(ofIndex: index)
+        let rightChildIndex = leftChildIndex + 1
+
+        var first = index
+        if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+            first = leftChildIndex
+        }
+        if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+            first = rightChildIndex
+        }
+        if first == index { return }
+
+        nodes.swapAt(index, first)
+        shiftDown(from: first, until: endIndex)
+    }
+
+    mutating func shiftDown(_ index: Int) {
+        shiftDown(from: index, until: nodes.count)
+    }
 }
+
+extension Heap where T: Equatable {
+    func index(of node: T) -> Int? {
+        return nodes.index(where: { $0 == node })
+    }
+
+    @discardableResult mutating func remove(node: T) -> T? {
+        if let index = index(of: node) {
+            return remove(at: index)
+        }
+        return nil
+    }
+}
+
+let heap = Heap(array: [1,2,3,4,5,6,7,8]) {
+    return $0 < $1
+}
+
+print(heap)
